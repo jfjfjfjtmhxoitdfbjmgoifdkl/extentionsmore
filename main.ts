fe15ca7333b8411856dd4a6bc452f0007e602a22
jfjfjfjtmhxoitdfbjmgoifdkl <124844533+jfjfjfjtmhxoitdfbjmgoifdkl@users.noreply.github.com>
2024-01-29 13:11:21 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,61 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    WaitUntilBlocks.waitUntilButtonPressed(Button.B)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
 basic.forever(function () {
 	
